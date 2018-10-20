f0039ea1243c8c77335e0d47f3cb95804c554239
Andrés Uribe Stengel <auribest@stud.hs-heilbronn.de>
2018-10-20 10:41:14 +0200

    Add four ArrayLists to the Football class.

--- src/main/java/de/exxcellent/challenge/Football.java
+++ src/main/java/de/exxcellent/challenge/Football.java
@@ -1,6 +1,17 @@
 package de.exxcellent.challenge;
 
+import java.util.ArrayList;
+
 public class Football {
   //Source of the football file.
   private String footballFile = "src/main/resources/de/exxcellent/challenge/football.csv";
+
+  //ArrayList to save all team names.
+  private ArrayList<String> teamNames = new ArrayList<>();
+  //ArrayList to save all absolute differences between goals & goals allowed.
+  private ArrayList<Integer> absoluteDiffOfGoals = new ArrayList<>();
+  //ArrayList to save all goals of each team.
+  private ArrayList<String> goals = new ArrayList();
+  //ArrayList to save all goals allowed of each team.
+  private ArrayList<String> goalsAllowed = new ArrayList<>();
 }
