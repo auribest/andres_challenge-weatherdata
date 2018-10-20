32fe1fe20b25e7e32aba95985f8d8dbed7ae96e0
Andrés Uribe Stengel <auribest@stud.hs-heilbronn.de>
2018-10-20 10:40:18 +0200

    Add the source of the football file.

--- src/main/java/de/exxcellent/challenge/Football.java
+++ src/main/java/de/exxcellent/challenge/Football.java
@@ -1,5 +1,6 @@
 package de.exxcellent.challenge;
 
 public class Football {
-  
+  //Source of the football file.
+  private String footballFile = "src/main/resources/de/exxcellent/challenge/football.csv";
 }
