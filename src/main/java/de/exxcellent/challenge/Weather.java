da2eea94abb7752d7bd894e6eeb33de5439475bf
Andrés Uribe Stengel <auribest@stud.hs-heilbronn.de>
2018-10-20 10:25:58 +0200

    Add the source of the weather file.

--- src/main/java/de/exxcellent/challenge/Weather.java
+++ src/main/java/de/exxcellent/challenge/Weather.java
@@ -1,5 +1,6 @@
 package de.exxcellent.challenge;
 
 public class Weather {
-  
+  //Source of the weather file.
+  private String weatherFile = "src/main/resources/de/exxcellent/challenge/weather.csv";
 }
