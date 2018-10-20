6c1c85b02e09419131dc0017e09a754e23ba2d64
Andrés Uribe Stengel <auribest@stud.hs-heilbronn.de>
2018-10-20 10:23:51 +0200

    Reformated code to fit google-checkstyles.

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -1,21 +1,25 @@
 package de.exxcellent.challenge;
 
 /**
- * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
+ * The entry class for your solution. This class is only aimed as starting point and
+ * not intended as baseline for your software
  * design. Read: create your own classes and packages as appropriate.
  *
- * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * @author Benjamin Schmid < benjamin.schmid@exxcellent.de >.
+ * @editor Andrés Uribe Stengel.
  */
 public final class App {
 
-    public static void main(String... args) {
+  /**
+   * The main method of the App.
+   * @param args The parameter for the main method.
+   */
+  public static void main(String... args) {
 
-        // Your preparation code …
+    String dayWithSmallestTempSpread = "Someday";
+    String teamWithSmallesGoalSpread = "A good team";
 
-        String dayWithSmallestTempSpread = "Someday";     // Your day analysis function call …
-        String teamWithSmallesGoalSpread = "A good team"; // Your goal analysis function call …
-
-        System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
-        System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallesGoalSpread);
-    }
+    System.out.printf("Day with smallest temperature spread : %s%n", dayWithSmallestTempSpread);
+    System.out.printf("Team with smallest goal spread       : %s%n", teamWithSmallesGoalSpread);
+  }
 }
